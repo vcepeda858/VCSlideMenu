51217b61da3ed200e34ac529ebb09375326899c9
Vincent Cepeda <vcepeda@cantorgaming.com>
2013-06-26 14:27:09 -0700

    Initial Commit

---
+++ VCSlideMenuDemo/VCAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  VCAppDelegate.h
+//  VCSlideMenuDemo
+//
+//  Created by Vincent Cepeda on 6/26/13.
+//  Copyright (c) 2013 Vincent Cepeda. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface VCAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ VCSlideMenuDemo/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  VCSlideMenuDemo
+//
+//  Created by Vincent Cepeda on 6/26/13.
+//  Copyright (c) 2013 Vincent Cepeda. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "VCAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([VCAppDelegate class]));
+    }
+}
